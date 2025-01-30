e1024df3c242c56622879187f9620dd48881e183
Anders Bruun Severinsen <202204885@post.au.dk>
2025-01-30 17:25:13 +0100

    Add new solution to Skak using dotnet

---
+++ Skák/dotnet/Program.cs
@@ -0,0 +1,32 @@
+﻿try
+{
+    string inp1 = Console.ReadLine();
+    string[] P = inp1.Split(' ');
+    string inp2 = Console.ReadLine();
+    string[] G = inp2.Split(' ');
+
+    int P_x = Convert.ToInt32(P[0]);
+    int P_y = Convert.ToInt32(P[1]);
+    int G_x = Convert.ToInt32(G[0]);
+    int G_y = Convert.ToInt32(G[1]);
+    int output = 0;
+    if (P_x != G_x) {
+        output++;
+    }
+    if (P_y != G_y) {
+        output++;
+    }
+    Console.WriteLine(output);
+}
+catch (Exception e)
+{
+    switch(e)
+    {
+        case System.IndexOutOfRangeException:
+            Console.WriteLine("Wrong input format!");
+            break;
+        default:
+            Console.WriteLine("Something went wrong!");
+            break;
+    }
+}
